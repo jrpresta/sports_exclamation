2bbcad883ff551bbbf92e02dbafa228b2379e721
Jon-Ross <jon_ross@Jon-Rosss-MacBook-Pro.local>
2019-03-23 12:06:38 -0700

    cleaned up rotoworld scraper

--- backend/code/scrapers/scrapers.py
+++ backend/code/scrapers/scrapers.py
@@ -16,17 +16,23 @@ def sports_line_scrape(file_dest):
 
 def roto_guru_scraper(service, month, day, year, file_dest):
     """Accesses the data table from SERVICE, with statistics
-    from MONTH-DAY-YEAR and saves to FILE_DEST"""
+    from MONTH-DAY-YEAR and saves to FILE_DEST
+    SERVICE will be fd for FanDuel and dk for DraftKings"""
+
     url = 'http://rotoguru1.com/cgi-bin/hyday.pl' +\
          f'?game={service}&mon={month}&day={day}&year={year}'
 
-    print(url)
     html = requests.get(url).content
-    # TODO: Clean up the rows that have the "jump to"
     df_list = pd.read_html(html)
     df = df_list[-2]
+    df.columns = ('position player points salary team opponent ' +\
+                  'score minutes stats').split()
+
+    # ensure that the rows are only
+    df = df[df['position'].apply(lambda x: len(x) < 3)]
     df.to_csv(file_dest, index=False)
 
+
 if __name__ == '__main__':
-    # sports_line_scrape('/tmp/basketball.csv')
+    sports_line_scrape('/tmp/basketball.csv')
     roto_guru_scraper('fd', 11, 21, 2018, '/tmp/roto.csv')
