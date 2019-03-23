7e45e3f52fc692cdc246e751d4f63d395aae8730
Jon-Ross <jon_ross@Jon-Rosss-MacBook-Pro.local>
2019-03-22 21:31:16 -0700

    added rotoguru scraper

---
+++ backend/code/bosun.py
@@ -0,0 +1,4 @@
+# The purpose of this bosun (short for boatswain) is to facilitate
+# the data collection process. We want to be alerted to any errors
+# as well as ensure that the correct data is being saved in the
+# correct files. This is the job of the bosun 
--- backend/code/scrapers/scrapers.py
+++ backend/code/scrapers/scrapers.py
@@ -1,5 +1,7 @@
 import wget
 import sites_for_scraping
+import pandas as pd
+import requests
 
 def sports_line_scrape(file_dest):
     """Downloads the csv stored at the url indiciated
@@ -12,5 +14,19 @@ def sports_line_scrape(file_dest):
         return 0
 
 
+def roto_guru_scraper(service, month, day, year, file_dest):
+    """Accesses the data table from SERVICE, with statistics
+    from MONTH-DAY-YEAR and saves to FILE_DEST"""
+    url = 'http://rotoguru1.com/cgi-bin/hyday.pl' +\
+         f'?game={service}&mon={month}&day={day}&year={year}'
+
+    print(url)
+    html = requests.get(url).content
+    # TODO: Clean up the rows that have the "jump to"
+    df_list = pd.read_html(html)
+    df = df_list[-2]
+    df.to_csv(file_dest, index=False)
+
 if __name__ == '__main__':
-    sports_line_scrape('/tmp/basketball.csv')
+    # sports_line_scrape('/tmp/basketball.csv')
+    roto_guru_scraper('fd', 11, 21, 2018, '/tmp/roto.csv')
